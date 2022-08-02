acecb9f33ddca40598ad46a1c02765d2ec627547
giihsatiko <giovana_satiko@hotmail.com>
2022-08-02 14:07:57 -0300

    correção nav

---
+++ footer.php
@@ -0,0 +1,169 @@
+<footer>
+  <div class="contato">
+    <div class="links-rodape container-fluid container-lg">
+      <div class="row">
+        <div class=" col-12 col-sm-4 col-lg-3">
+          <div class="atendimentos-container">
+            <div class="atendimento">
+              <h4 class="fw600 fs18">Atendimento</h4>
+              <div>
+                <img src="assets/images/local.png" alt="pin localização">
+                <p class="fw400 fs18">Seg à Sex das 8h às 18hrs</p>
+              </div>
+            </div>
+            <div class="atendimento">
+              <h4 class="fw600 fs18">Central de Atendimento</h4>
+              <div>
+                <img src="assets/images/tel.png" alt="telefone">
+                <p>18 3916 2287</p>
+              </div>
+            </div>
+            <div class="d-sm-none d-block">
+              <ul class="d-flex justify-content-around w-100 py-5">
+                <li><a href=""><svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.4991 15C10.4974 15 10.4954 15 10.4933 15C9.85965 14.9955 4.25797 14.9452 2.67088 14.5211C1.56172 14.2266 0.686394 13.3618 0.387221 12.2654C-0.0282507 10.7243 -0.00153886 7.75733 0.0013378 7.52051C-0.00153886 7.28491 -0.0282507 4.2932 0.385988 2.73946C0.386399 2.73783 0.38681 2.73662 0.387221 2.73499C0.683106 1.65123 1.57816 0.759607 2.66841 0.464294C2.67088 0.463482 2.67375 0.46267 2.67663 0.462263C4.24564 0.0540255 9.85883 0.00446828 10.4937 0H10.5053C11.1406 0.00446828 16.7571 0.0548379 18.3298 0.47973C19.4361 0.773418 20.311 1.63701 20.6105 2.73215C21.0412 4.28711 21.0014 7.28491 20.9973 7.53798C21.0001 7.78739 21.0256 10.7275 20.6126 12.2764C20.6122 12.278 20.6118 12.2792 20.6114 12.2809C20.3122 13.3772 19.4369 14.2416 18.3265 14.5369C18.3248 14.5373 18.3236 14.5377 18.322 14.5381C16.7529 14.9456 11.1398 14.9955 10.5053 15C10.5032 15 10.5011 15 10.4991 15ZM1.97185 3.15501C1.60692 4.52636 1.64144 7.47989 1.64186 7.50955V7.53107C1.63117 8.34999 1.66939 10.7222 1.97185 11.8454C2.11856 12.3832 2.55047 12.8093 3.09786 12.9552C4.26907 13.2683 8.83104 13.3666 10.4987 13.3784C12.1704 13.3666 16.7398 13.2712 17.902 12.9706C18.4477 12.8248 18.878 12.3999 19.0259 11.8612C19.3292 10.7214 19.367 8.36096 19.3559 7.54692C19.3559 7.53839 19.3559 7.52986 19.3559 7.52133C19.3711 6.69226 19.3411 4.29279 19.0272 3.16029C19.0267 3.15907 19.0263 3.15826 19.0263 3.15704C18.8788 2.61678 18.4469 2.19067 17.8995 2.04485C16.7398 1.73166 12.17 1.63336 10.4987 1.62158C8.82775 1.63336 4.26331 1.72882 3.09745 2.029C2.56074 2.17646 2.11897 2.618 1.97185 3.15501ZM19.8191 12.07H19.8195H19.8191ZM8.40817 10.7836V4.21602L14.1504 7.4998L8.40817 10.7836Z" fill="black" />
+                    </svg>
+                  </a></li>
+                <li><a href=""><svg width="8" height="16" viewBox="0 0 8 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M8 5.5H5V3.5C5 2.94812 5.44812 2.5 6 2.5H7V0H5C3.34312 0 2 1.34312 2 3V5.5H0V8H2V16H5V8H7L8 5.5Z" fill="black" />
+                    </svg></a></li>
+                <li><a href=""><svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M12.589 0H5.72228C2.56251 0 0 2.64281 0 5.90159V12.9835C0 16.2423 2.56251 18.8851 5.72228 18.8851H12.589C15.7488 18.8851 18.3113 16.2423 18.3113 12.9835V5.90159C18.3113 2.64281 15.7488 0 12.589 0ZM16.5946 12.9835C16.5946 15.2615 14.7978 17.1146 12.589 17.1146H5.72228C3.51348 17.1146 1.71668 15.2615 1.71668 12.9835V5.90159C1.71668 3.62358 3.51348 1.77048 5.72228 1.77048H12.589C14.7978 1.77048 16.5946 3.62358 16.5946 5.90159V12.9835Z" fill="black" />
+                      <path d="M9.15558 4.72119C6.62741 4.72119 4.57776 6.83507 4.57776 9.44247C4.57776 12.0499 6.62741 14.1637 9.15558 14.1637C11.6838 14.1637 13.7334 12.0499 13.7334 9.44247C13.7334 6.83507 11.6838 4.72119 9.15558 4.72119ZM9.15558 12.3933C7.57838 12.3933 6.29444 11.0691 6.29444 9.44247C6.29444 7.81473 7.57838 6.49167 9.15558 6.49167C10.7328 6.49167 12.0167 7.81473 12.0167 9.44247C12.0167 11.0691 10.7328 12.3933 9.15558 12.3933Z" fill="black" />
+                      <path d="M14.1913 4.72124C14.4441 4.72124 14.6491 4.50986 14.6491 4.24911C14.6491 3.98836 14.4441 3.77698 14.1913 3.77698C13.9385 3.77698 13.7335 3.98836 13.7335 4.24911C13.7335 4.50986 13.9385 4.72124 14.1913 4.72124Z" fill="black" />
+                    </svg>
+                  </a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="links-itens col-8 col-lg-6">
+          <div class="row">
+            <div class="col-12 col-sm-5">
+              <a href="index.php"><svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1 1L9 8.5L1 16" stroke="black" stroke-width="1.25" />
+                </svg> HOME</a>
+              <a href="blog.php"><svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1 1L9 8.5L1 16" stroke="black" stroke-width="1.25" />
+                </svg> BLOG</a>
+              <a href="contato.php"><svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1 1L9 8.5L1 16" stroke="black" stroke-width="1.25" />
+                </svg> CONTATO</a>
+              <a href=""><svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1 1L9 8.5L1 16" stroke="black" stroke-width="1.25" />
+                </svg> TRABALHE CONOSCO</a>
+            </div>
+            <div class="col-12 col-sm-7">
+              <a href="empreendimentos.php"><svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1 1L9 8.5L1 16" stroke="black" stroke-width="1.25" />
+                </svg> EMPREENDIMENTOS</a>
+              <a href="#"><svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1 1L9 8.5L1 16" stroke="black" stroke-width="1.25" />
+                </svg> OBRAS CONCLUÍDAS</a>
+              <a href="#"><svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1 1L9 8.5L1 16" stroke="black" stroke-width="1.25" />
+                </svg> OBRAS EM ANDAMENTOS</a>
+              <a href=""><svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1 1L9 8.5L1 16" stroke="black" stroke-width="1.25" />
+                </svg> OFEREÇA SUA ÁREA</a>
+            </div>
+            <div class="col-12 pt-5">
+              <div class="politica">
+                <a href="">Política de Privacidade</a>
+                <a href="">Preferência de Cookies</a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="botoes col-12 col-lg-3 align-items-center">
+          <div class="row w-100 justify-content-between justify-content-lg-end">
+            <div class="col-lg-12 col-sm-9 d-flex flex-lg-column align-items-center caixa-botoes">
+              <a href="" class="botao-preto"><img src="assets/images/grupo.png" alt="grupo"> COLABORADOR</a>
+              <a href="" class="botao-vermelho fw600"><img src="assets/images/usuario.png" alt="usuario"> CLIENTE</a>
+            </div>
+            <div class="col-lg-12 col-sm-3 d-none d-sm-flex">
+              <ul>
+                <li><a href=""><svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.4991 15C10.4974 15 10.4954 15 10.4933 15C9.85965 14.9955 4.25797 14.9452 2.67088 14.5211C1.56172 14.2266 0.686394 13.3618 0.387221 12.2654C-0.0282507 10.7243 -0.00153886 7.75733 0.0013378 7.52051C-0.00153886 7.28491 -0.0282507 4.2932 0.385988 2.73946C0.386399 2.73783 0.38681 2.73662 0.387221 2.73499C0.683106 1.65123 1.57816 0.759607 2.66841 0.464294C2.67088 0.463482 2.67375 0.46267 2.67663 0.462263C4.24564 0.0540255 9.85883 0.00446828 10.4937 0H10.5053C11.1406 0.00446828 16.7571 0.0548379 18.3298 0.47973C19.4361 0.773418 20.311 1.63701 20.6105 2.73215C21.0412 4.28711 21.0014 7.28491 20.9973 7.53798C21.0001 7.78739 21.0256 10.7275 20.6126 12.2764C20.6122 12.278 20.6118 12.2792 20.6114 12.2809C20.3122 13.3772 19.4369 14.2416 18.3265 14.5369C18.3248 14.5373 18.3236 14.5377 18.322 14.5381C16.7529 14.9456 11.1398 14.9955 10.5053 15C10.5032 15 10.5011 15 10.4991 15ZM1.97185 3.15501C1.60692 4.52636 1.64144 7.47989 1.64186 7.50955V7.53107C1.63117 8.34999 1.66939 10.7222 1.97185 11.8454C2.11856 12.3832 2.55047 12.8093 3.09786 12.9552C4.26907 13.2683 8.83104 13.3666 10.4987 13.3784C12.1704 13.3666 16.7398 13.2712 17.902 12.9706C18.4477 12.8248 18.878 12.3999 19.0259 11.8612C19.3292 10.7214 19.367 8.36096 19.3559 7.54692C19.3559 7.53839 19.3559 7.52986 19.3559 7.52133C19.3711 6.69226 19.3411 4.29279 19.0272 3.16029C19.0267 3.15907 19.0263 3.15826 19.0263 3.15704C18.8788 2.61678 18.4469 2.19067 17.8995 2.04485C16.7398 1.73166 12.17 1.63336 10.4987 1.62158C8.82775 1.63336 4.26331 1.72882 3.09745 2.029C2.56074 2.17646 2.11897 2.618 1.97185 3.15501ZM19.8191 12.07H19.8195H19.8191ZM8.40817 10.7836V4.21602L14.1504 7.4998L8.40817 10.7836Z" fill="black" />
+                    </svg>
+                  </a></li>
+                <li><a href=""><svg width="8" height="16" viewBox="0 0 8 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M8 5.5H5V3.5C5 2.94812 5.44812 2.5 6 2.5H7V0H5C3.34312 0 2 1.34312 2 3V5.5H0V8H2V16H5V8H7L8 5.5Z" fill="black" />
+                    </svg></a></li>
+                <li><a href=""><svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M12.589 0H5.72228C2.56251 0 0 2.64281 0 5.90159V12.9835C0 16.2423 2.56251 18.8851 5.72228 18.8851H12.589C15.7488 18.8851 18.3113 16.2423 18.3113 12.9835V5.90159C18.3113 2.64281 15.7488 0 12.589 0ZM16.5946 12.9835C16.5946 15.2615 14.7978 17.1146 12.589 17.1146H5.72228C3.51348 17.1146 1.71668 15.2615 1.71668 12.9835V5.90159C1.71668 3.62358 3.51348 1.77048 5.72228 1.77048H12.589C14.7978 1.77048 16.5946 3.62358 16.5946 5.90159V12.9835Z" fill="black" />
+                      <path d="M9.15558 4.72119C6.62741 4.72119 4.57776 6.83507 4.57776 9.44247C4.57776 12.0499 6.62741 14.1637 9.15558 14.1637C11.6838 14.1637 13.7334 12.0499 13.7334 9.44247C13.7334 6.83507 11.6838 4.72119 9.15558 4.72119ZM9.15558 12.3933C7.57838 12.3933 6.29444 11.0691 6.29444 9.44247C6.29444 7.81473 7.57838 6.49167 9.15558 6.49167C10.7328 6.49167 12.0167 7.81473 12.0167 9.44247C12.0167 11.0691 10.7328 12.3933 9.15558 12.3933Z" fill="black" />
+                      <path d="M14.1913 4.72124C14.4441 4.72124 14.6491 4.50986 14.6491 4.24911C14.6491 3.98836 14.4441 3.77698 14.1913 3.77698C13.9385 3.77698 13.7335 3.98836 13.7335 4.24911C13.7335 4.50986 13.9385 4.72124 14.1913 4.72124Z" fill="black" />
+                    </svg>
+                  </a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="direitos">
+        <img src="assets/images/logo-horizontal.png" alt="logo">
+        <p class="fw500 fs14">
+          Todos os direitos reservados © 2021 | AS Incorporadora - CRECI 021030-J - Criação
+          <img src="assets/images/oxigenweb.png" alt="logo oxigenweb">
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="rodape">
+    <div class="container-fluid container-lg">
+      <div class="contato-rodape">
+        <a href="">
+          <img src="assets/images/carta.png" alt="carta">
+          <p class="fw500 fs18 fs14-sm"><span class="fw700">Formulário</span> de contato</p>
+        </a>
+        <a href="">
+          <img src="assets/images/tel2.png" alt="telefone">
+          <p class="fw500 fs18 fs14-sm"><span class="fw700">Vendas</span> 18 3916 2287</p>
+        </a>
+        <a href="">
+          <img src="assets/images/whatsapp.png" alt="whatsapp">
+          <p class="fw500 fs18 fs14-sm"><span class="fw700">Whatsapp</span> 18 3916 2287</p>
+        </a>
+      </div>
+    </div>
+  </div>
+
+</footer>
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<script>
+  var swiper = new Swiper(".mySwiper-apresentacao", {
+    navigation: {
+      nextEl: ".apresentacao-button-next",
+      prevEl: ".apresentacao-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+</script>
+
+<script>
+  jQuery('document').ready(function() {
+    jQuery('.links-menu').addClass('d-none')
+    jQuery('.menu').on('click', function() {
+      if (jQuery('.menu').hasClass('active')) {
+        jQuery('.menu').removeClass('active');
+        jQuery('.links-menu').addClass('d-none');
+        jQuery('body').removeClass('overflow-hidden');
+      } else {
+        jQuery('.menu').addClass('active');
+        jQuery('.links-menu').removeClass('d-none');
+        jQuery('body').addClass('overflow-hidden');
+      }
+    });
+  });
+</script>
+
+</body>
+
+</html>
--- header.php
+++ header.php
@@ -8,6 +8,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   <script src="https://kit.fontawesome.com/71123056be.js" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -27,7 +28,7 @@
         <a href="index.php"><img src="images/logo.png" alt="logo página"></a>
         <div class="itens-nav">
           <a href="#" class="lupa"><img src="images/lupa.png" alt="lupa"></a>
-          <a href="#" class="botao-laranja">
+          <a href="#" class="botao-laranja px-3">
             <img src="images/user.png" alt="usuário">
             Login | Criar conta
           </a>
@@ -37,33 +38,49 @@
               <span class="mid"></span>
               <span class="bot"></span>
             </div>
-            <p>MENU</p>
+            <p class="fw-600 fs-18">MENU</p>
           </div>
-
-          <div class="links-menu">
-            <div class="itens-1">
+        </div>
+        <div class="links-menu">
+          <div class="itens-1 col-12 col-lg-5">
+            <div class="links-1 fw-400 fs-18">
               <a href="">
                 <img src="images/seta-lado-branca.svg" alt="">
+                <p>HOME</p>
               </a>
               <a href="">
                 <img src="images/seta-lado-branca.svg" alt="">
+                <p>INSTITUCIONAL</p>
               </a>
               <a href="">
                 <img src="images/seta-lado-branca.svg" alt="">
+                <p>NOVIDADES</p>
               </a>
               <a href="">
                 <img src="images/seta-lado-branca.svg" alt="">
+                <p>MATERIAIS GRATUITOS</p>
               </a>
               <a href="">
                 <img src="images/seta-lado-branca.svg" alt="">
+                <p>DEPOIMENTOS</p>
               </a>
               <a href="">
                 <img src="images/seta-lado-branca.svg" alt="">
+                <p>CONTATO</p>
               </a>
             </div>
           </div>
+          <div class="itens-2 col-12 col-lg-7">
+            <div class="links-2 fw-400 fs-27">
+              <a href="">Cursos de Direito</a>
+              <a href="">Cursos de Psicologia</a>
+              <a href="">Cursos de Serviço Social</a>
+              <a href="">Cursos Preparatórios</a>
+              <a href="">Coaching e Inteligência Emocional</a>
+              <a href="">Cursos Para sua Empresa</a>
+            </div>
+          </div>
         </div>
-
       </div>
     </div>
   </header>
